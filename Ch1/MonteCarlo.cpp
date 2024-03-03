289b55ee1c5a81b8a4451f7db990d0ba559cb4cc
Jacob <jdennon@ucsc.edu>
2024-03-03 11:13:29 -0800

    Working on Monte Carlo for vanilla option

--- Ch1/MonteCarlo.cpp
+++ Ch1/MonteCarlo.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
 int main() {
+    float RiskFreeReturn, ExpiryDate, InitialPrice, Sigma;
+
+    // Ask user for constant values
+    std::cout << "Please provide a risk free return rate: " << std::endl;
+    // Check if value is of correct type
+    if(!(std::cin >> RiskFreeReturn)) {
+        std::cout << "Invalid input. Exiting Program" << std::endl;
+        return 0;
+    }
+
+    std::cout << "Please provide an expiry date: " << std::endl;
+    if(!(std::cin >> ExpiryDate)) {
+        std::cout << "Invalid input. Exiting Program" << std::endl;
+        return 0;
+    }
+
+    std::cout << "Please provide an initial price: " << std::endl;
+    if(!(std::cin >> InitialPrice)) {
+        std::cout << "Invalid input. Exiting Program" << std::endl;
+        return 0;
+    }
+
+    std::cout << "Please provide a sigma: " << std::endl;
+    if(!(std::cin >> Sigma)) {
+        std::cout << "Invalid input. Exiting Program" << std::endl;
+        return 0;
+    }
+
     return 0;
 }
--- makefile
+++ makefile
@@ -1,21 +1,24 @@
-#Compiler
+# Compiler
 CXX = g++
 CXXFLAGS = -std=c++11 -Wall
+
 
 # Exec names for targets
 HELLO_WORLD = HelloWorld
 MONTECARLO = Ch1/MonteCarlo
-
 #Source files
 HELLO_SRCS = HelloWorld.cpp
-MONTECARLO_SRCS = MonteCarlo.cpp
+MONTECARLO_SRCS = Ch1/MonteCarlo.cpp
 
-#Make All
+
+# Make All
 all: $(HELLO_WORLD) $(MONTECARLO)
 
+
+# Specific Makes
 # Make HelloWorld
 $(HELLO_WORLD): $(HELLO_SRCS)
 	$(CXX) $(CXXFLAGS) -o $@ $(HELLO_SRCS)
-
+# Make Montecarlo
 $(MONTECARLO): $(MONTECARLO_SRCS)
 	$(CXX) $(CXXFLAGS) -o $@ $(MONTECARLO_SRCS)
