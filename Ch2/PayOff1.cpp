2dc8d00bbce8119977b340ae8aefd09dce3c054a
Jacob <jdennon@ucsc.edu>
2024-03-22 18:51:34 -0700

    Created class source

--- Ch2/PayOff1.cpp
+++ Ch2/PayOff1.cpp
@@ -1,3 +1,23 @@
 #include "./PayOff1.h"
 #include "../Common/MinMax.h"
 
+PayOff::PayOff(double Strike_, OptionType TheOptionsType_)
+:
+    Strike(Strike_), TheOptionsType(TheOptionsType_)
+{
+}
+
+double PayOff::operator ()(double Spot) const
+{
+    switch (TheOptionsType)
+    {
+        case call :
+            return Max(Spot - Strike, 0.0);
+
+        case put:
+            return Max(Strike - Spot, 0.0);
+
+        default:
+            throw("Unknown option type found.");
+    }
+}
